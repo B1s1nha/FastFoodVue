773cdb308ac299cd689eaccc20dacd595d92d3c2
Gabriella Huppes <b1s1nha_@hotmail.com>
2023-05-12 11:42:32 -0300

    I changed products place, started to use real vue, using v-for and v-on, here we have a little problem about src, but later I will repair my mistake

--- src/App.vue
+++ src/App.vue
@@ -14,73 +14,9 @@ export default {
   },
   data() {
     return {
-        products: [
-              {
-                  "photo": "../assets/img/hamburguermeat.png",
-                  "name": "Meat Burguer",
-                  "price": 5.99,
-                  "active": false,
-                  "quantity": 1
-              },
-              {
-                  "photo": "../assets/img/hamburguerchicken.png",
-                  "name": "Chicken Burguer",
-                  "price": 4.99,
-                  "active": false,
-                  "quantity": 1
-              },
-              {
-                  "photo": "../assets/img/hamburguerbacon.png",
-                  "name": "Bacon Burguer",
-                  "price": 2.99,
-                  "active": false,
-                  "quantity": 1
-              },
-              {
-                  "photo": "../assets/img/fries.png",
-                  "name": "Fries",
-                  "price": 2.99,
-                  "active": false,
-                  "quantity": 1
-              },
-              {
-                  "photo": "../assets/img/nuggets.png",
-                  "name": "Nuggets",
-                  "price": 3.49,
-                  "active": false,
-                  "quantity": 1
-              },
-              {
-                  "photo": "../assets/img/cookies.png",
-                  "name": "cookies",
-                  "price": 2.79,
-                  "active": false,
-                  "quantity": 1
-              },
-              {
-                  "photo": "../assets/img/soda.png",
-                  "name": "Mango Soda",
-                  "price": 1.99,
-                  "active": false,
-                  "quantity": 1
-              },
-              {
-                  "photo": "../assets/img/icetea.png",
-                  "name": "Ice Tea",
-                  "price": 1.99,
-                  "active": false,
-                  "quantity": 1
-              },
-              {
-                  "photo": "../assets/img/water.png",
-                  "name": "Water",
-                  "price": 1.49,
-                  "active": false,
-                  "quantity": 1
-              }
-        ],
+        message1: "nothing",
     }
-  }
+  },
 }
 </script>
 
--- src/components/FoodList.vue
+++ src/components/FoodList.vue
@@ -1,173 +1,126 @@
 <template>
   <main>
     <section class="items">
-        <h4>{{message}}</h4>
-        <div class="product selected">
-          <div class="photo">
-            <img src="../assets/img/hamburguermeat.png" />
-          </div>
-          <div class="description">
-            <span class="name">Meat Burguer</span>
-            <span class="price">$ 5.99</span>
-            <div class="quantity-area">
-              <button><box-icon name="minus" size="xs"></box-icon></button>
-              <span class="quantity">1</span>
-              <button><box-icon name="plus" size="xs"></box-icon></button>
-            </div>
+      <h4>{{ message }}</h4>
+      <div 
+      v-for="product in products" 
+      :key="product.id" 
+      class="product"
+      v-on:click="product.active = !product.active"
+      :class="product.active ? 'selected' : ''"
+      >
+        <div class="photo">
+          <img :src="product.photo" />
+        </div>
+        <div class="description">
+          <span class="name"> {{ product.name }} </span>
+          <span class="price">{{ product.price }}</span>
+          <div class="quantity-area">
+            <button><box-icon name="minus" size="xs"></box-icon></button>
+            <span class="quantity">{{ product.quantity }}</span>
+            <button><box-icon name="plus" size="xs"></box-icon></button>
           </div>
         </div>
-        <div class="product">
-          <div class="photo">
-            <img src="../assets/img/hamburguerchicken.png" />
-          </div>
-          <div class="description">
-            <span class="name">Chicken Burguer</span>
-            <span class="price">$ 4.99</span>
-            <div class="quantity-area">
-              <button><box-icon name="minus" size="xs"></box-icon></button>
-              <span class="quantity">1</span>
-              <button><box-icon name="plus" size="xs"></box-icon></button>
-            </div>
-          </div>
-        </div>
-        <div class="product">
-          <div class="photo">
-            <img src="../assets/img/hamburguerbacon.png" />
-          </div>
-          <div class="description">
-            <span class="name">Bacon Burguer</span>
-            <span class="price">$ 2.99</span>
-            <div class="quantity-area">
-              <button><box-icon name="minus" size="xs"></box-icon></button>
-              <span class="quantity">1</span>
-              <button><box-icon name="plus" size="xs"></box-icon></button>
-            </div>
-          </div>
-        </div>
-        <div class="product">
-          <div class="photo">
-            <img src="../assets/img/fries.png" />
-          </div>
-          <div class="description">
-            <span class="name">Fries</span>
-            <span class="price">$ 2.99</span>
-            <div class="quantity-area">
-              <button><box-icon name="minus" size="xs"></box-icon></button>
-              <span class="quantity">1</span>
-              <button><box-icon name="plus" size="xs"></box-icon></button>
-            </div>
-          </div>
-        </div>
-        <div class="product">
-          <div class="photo">
-            <img src="../assets/img/nuggets.png" />
-          </div>
-          <div class="description">
-            <span class="name">Nuggets</span>
-            <span class="price">$ 3.49</span>
-            <div class="quantity-area">
-              <button><box-icon name="minus" size="xs"></box-icon></button>
-              <span class="quantity">1</span>
-              <button><box-icon name="plus" size="xs"></box-icon></button>
-            </div>
-          </div>
-        </div>
-        <div class="product">
-          <div class="photo">
-            <img src="../assets/img/cookie.png" />
-          </div>
-          <div class="description">
-            <span class="name">Cookie</span>
-            <span class="price">$ 2.79</span>
-            <div class="quantity-area">
-              <button><box-icon name="minus" size="xs"></box-icon></button>
-              <span class="quantity">1</span>
-              <button><box-icon name="plus" size="xs"></box-icon></button>
-            </div>
-          </div>
-        </div>
-        <div class="product">
-          <div class="photo">
-            <img src="../assets/img/soda.png" />
-          </div>
-          <div class="description">
-            <span class="name">Mango Soda</span>
-            <span class="price">$ 1.99</span>
-            <div class="quantity-area">
-              <button><box-icon name="minus" size="xs"></box-icon></button>
-              <span class="quantity">1</span>
-              <button><box-icon name="plus" size="xs"></box-icon></button>
-            </div>
-          </div>
-        </div>
-        <div class="product">
-          <div class="photo">
-            <img src="../assets/img/icetea.png" />
-          </div>
-          <div class="description">
-            <span class="name">Ice Tea</span>
-            <span class="price">$ 1.99</span>
-            <div class="quantity-area">
-              <button><box-icon name="minus" size="xs"></box-icon></button>
-              <span class="quantity">1</span>
-              <button><box-icon name="plus" size="xs"></box-icon></button>
-            </div>
-          </div>
-        </div>
-        <div class="product">
-          <div class="photo">
-            <img src="../assets/img/water.png" />
-          </div>
-          <div class="description">
-            <span class="name">Water</span>
-            <span class="price">$ 1.49</span>
-            <div class="quantity-area">
-              <button><box-icon name="minus" size="xs"></box-icon></button>
-              <span class="quantity">1</span>
-              <button><box-icon name="plus" size="xs"></box-icon></button>
-            </div>
-          </div>
-        </div>
-      </section>
+      </div>
+    </section>
 
-      <section class="summary">
-        <strong>Resumo do pedido</strong>
-        <table>
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Meat Burguer</td>
-              <td>5.99</td>
-            </tr>
+    <section class="summary">
+      <strong>Resumo do pedido</strong>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Meat Burguer</td>
+            <td>5.99</td>
+          </tr>
 
-            <tr>
-              <th>Total</th>
-              <th>5.99</th>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-    <section class="comment">
-        <box-icon name='quote-alt-left' type='solid' size="xs" ></box-icon>
-        Food to buy
-        <box-icon name='quote-alt-right' type='solid' size="xs"></box-icon>
-        </section>
-        </main>
+          <tr>
+            <th>Total</th>
+            <th>5.99</th>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  </main>
 </template>
 
 <script>
-
 export default {
   name: "FoodList",
-  data(){
-    return{
-      message: "Make your food choice!"
-    }
-  }
+  data() {
+    return {
+      message: "Make your food choice!",
+      products: [
+              {
+                  "photo": "../assets/img/hamburguermeat.png",
+                  "name": "Meat Burguer",
+                  "price": 5.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/hamburguerchicken.png",
+                  "name": "Chicken Burguer",
+                  "price": 4.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/hamburguerbacon.png",
+                  "name": "Bacon Burguer",
+                  "price": 2.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/fries.png",
+                  "name": "Fries",
+                  "price": 2.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/nuggets.png",
+                  "name": "Nuggets",
+                  "price": 3.49,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/cookies.png",
+                  "name": "cookies",
+                  "price": 2.79,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/soda.png",
+                  "name": "Mango Soda",
+                  "price": 1.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/icetea.png",
+                  "name": "Ice Tea",
+                  "price": 1.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/water.png",
+                  "name": "Water",
+                  "price": 1.49,
+                  "active": false,
+                  "quantity": 1
+              }
+        ],
+    };
+  },
 };
 </script>
