26f6d82719e0800e73c4e175713eea5538539fda
Gabriella Huppes <b1s1nha_@hotmail.com>
2023-05-12 08:52:55 -0300

    I changed component list name and made a jason file in App

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,76 @@ export default {
   name: 'App',
   components: {
     FoodList
+  },
+  data() {
+    return {
+        products: [
+              {
+                  "photo": "../assets/img/hamburguermeat.png",
+                  "name": "Meat Burguer",
+                  "price": 5.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/hamburguerchicken.png",
+                  "name": "Chicken Burguer",
+                  "price": 4.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/hamburguerbacon.png",
+                  "name": "Bacon Burguer",
+                  "price": 2.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/fries.png",
+                  "name": "Fries",
+                  "price": 2.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/nuggets.png",
+                  "name": "Nuggets",
+                  "price": 3.49,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/cookies.png",
+                  "name": "cookies",
+                  "price": 2.79,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/soda.png",
+                  "name": "Mango Soda",
+                  "price": 1.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/icetea.png",
+                  "name": "Ice Tea",
+                  "price": 1.99,
+                  "active": false,
+                  "quantity": 1
+              },
+              {
+                  "photo": "../assets/img/water.png",
+                  "name": "Water",
+                  "price": 1.49,
+                  "active": false,
+                  "quantity": 1
+              }
+        ],
+    }
   }
 }
 </script>
 
-
-<!-- <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
--- src/components/FoodList.vue
+++ src/components/FoodList.vue
@@ -161,6 +161,7 @@
 </template>
 
 <script>
+
 export default {
   name: "FoodList",
   data(){
@@ -170,21 +171,3 @@ export default {
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<!-- <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style> -->
